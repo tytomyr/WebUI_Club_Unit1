f8012d21a86ce723502ec86b0f614f7c83f23cdb
tytomyr <art.tytomyr@Gmail.com>
2021-08-30 04:40:39 +0000

    editing

---
+++ dom.js
@@ -0,0 +1,9 @@
+/*var box = document.getElementById('box');
+box.addEventListener('mousemove',runEvent);
+//box.addEventListener('mouseover',runEvent);
+let e;
+function runEvent(e){
+console.log('EVENT TYPE: '+  e.type);
+
+}*/
+//console.dir(document);
